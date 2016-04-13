67afb8077c05bbd4d369a4ea49d9b8370596cf72
Rodrigo Martins <rodrigo@rrmartins.com>
2016-04-13 10:49:02 -0300

    spec instance of Analyser'

--- spec/app/analyser_spec.rb
+++ spec/app/analyser_spec.rb
@@ -4,4 +4,18 @@ describe Analyser do
   context "error formated xml from source" do
     it {expect{ Analyser.new('foo') }.to raise_error(Nokogiri::XML::SyntaxError)}
   end
+
+  context "success" do
+    let(:analyser) { Analyser.new(@xml) }
+
+    context "#new" do
+      before(:each) do
+        @xml = "<PLAY><SPEECH><SPEAKER>Bob</SPEAKER>"
+        @xml += "<LINE>Hello</LINE><LINE>Bye</LINE></SPEECH></PLAY>"
+      end
+      it "should analyser is Analyser class" do
+        expect(analyser.class).to eq Analyser
+      end
+    end
+  end
 end
