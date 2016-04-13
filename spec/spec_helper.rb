3fb718757c121303085fa34a1de9e19a38b08962
Rodrigo Martins <rodrigo@rrmartins.com>
2016-04-13 10:47:03 -0300

    remove config deprecated

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,5 @@ require './app/analyser.rb'
 RSpec.configure do |config|
   config.order = :random
   config.run_all_when_everything_filtered = true
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.color = true
 end
