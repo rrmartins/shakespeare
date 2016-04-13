a386c2ac999b7cc3fe749dc7ed27cb48f36d9fb5
Rodrigo Martins <rodrigo@rrmartins.com>
2016-04-13 10:54:40 -0300

    finish spec of Analyser

--- spec/app/analyser_spec.rb
+++ spec/app/analyser_spec.rb
@@ -30,7 +30,69 @@ describe Analyser do
         end
       end
 
-      
+      context "aggregates counts for each speech of Bob" do
+        before(:each) do
+          @xml = "<PLAY><SPEECH><SPEAKER>Bob</SPEAKER>"
+          @xml += "<LINE>Hello</LINE><LINE>Bye</LINE></SPEECH>"
+          @xml += "<SPEECH><SPEAKER>Bob</SPEAKER>"
+          @xml += "<LINE>Hello</LINE><LINE>Bye</LINE></SPEECH></PLAY>"
+        end
+
+        it "should return speaker Bob and 4 lines" do
+          expect(analyser.analyze_macbeth).to eq "4 Bob"
+        end
+      end
+
+      context "aggregates counts for each speech by each character" do
+        before(:each) do
+          @xml = "<PLAY><SPEECH><SPEAKER>Bob</SPEAKER>"
+          @xml += "<LINE>Hello</LINE><LINE>Bye</LINE></SPEECH>"
+          @xml += "<SPEECH><SPEAKER>Fred</SPEAKER>"
+          @xml += "<LINE>Hello</LINE><LINE>Bye</LINE></SPEECH>"
+          @xml += "<SPEECH><SPEAKER>Bob</SPEAKER>"
+          @xml += "<LINE>Hello</LINE><LINE>Bye</LINE></SPEECH></PLAY>"
+        end
+
+        it "should return speaker Bob and 4 lines and Fred 2" do
+          expect(analyser.analyze_macbeth).to eq "4 Bob\n2 Fred"
+        end
+      end
+
+      context "excludes the 'ALL' character" do
+        before(:each) do
+          @xml = "<PLAY><SPEECH><SPEAKER>Bob</SPEAKER>"
+          @xml += "<LINE>Hello</LINE><LINE>Bye</LINE></SPEECH>"
+          @xml += "<SPEECH><SPEAKER>Fred</SPEAKER>"
+          @xml += "<LINE>Hello</LINE><LINE>Bye</LINE></SPEECH>"
+          @xml += "<SPEECH><SPEAKER>ALL</SPEAKER>"
+          @xml += "<LINE>Hello</LINE><LINE>Bye</LINE></SPEECH>"
+          @xml += "<SPEECH><SPEAKER>Bob</SPEAKER>"
+          @xml += "<LINE>Hello</LINE><LINE>Bye</LINE></SPEECH></PLAY>"
+        end
+
+        it "should return speaker Bob and 4 lines and Fred 2" do
+          expect(analyser.analyze_macbeth).to eq "4 Bob\n2 Fred"
+        end
+      end
+
+      context "displays results in descending line count order" do
+        before(:each) do
+          @xml = "<PLAY><SPEECH><SPEAKER>Bob</SPEAKER>"
+          @xml += "<LINE>Hello</LINE><LINE>Bye</LINE></SPEECH>"
+          @xml += "<SPEECH><SPEAKER>Fred</SPEAKER>"
+          @xml += "<LINE>Hello</LINE><LINE>Bye</LINE></SPEECH>"
+          @xml += "<SPEECH><SPEAKER>ALL</SPEAKER>"
+          @xml += "<LINE>Hello</LINE><LINE>Bye</LINE></SPEECH>"
+          @xml += "<SPEECH><SPEAKER>Bill</SPEAKER>"
+          @xml += "<LINE>Hello</LINE><LINE>Bye</LINE><LINE>Friends</LINE></SPEECH>"
+          @xml += "<SPEECH><SPEAKER>Bob</SPEAKER>"
+          @xml += "<LINE>Hello</LINE><LINE>Bye</LINE></SPEECH></PLAY>"
+        end
+
+        it "should return in order" do
+          expect(analyser.analyze_macbeth).to eq "4 Bob\n3 Bill\n2 Fred"
+        end
+      end
     end
   end
 end
