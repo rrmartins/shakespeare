337b571d2c694061600a4962526714e05fc865cc
Rodrigo Martins <rodrigo@rrmartins.com>
2016-04-13 10:03:03 -0300

    add news config's in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,7 @@ require 'nokogiri'
 
 RSpec.configure do |config|
   config.order = :random
+  config.run_all_when_everything_filtered = true
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.color = true
 end
